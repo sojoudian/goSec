c399002604c1031eb05bf29efef90f16f02b6d13
sojoudian <sojodyan@gmail.com>
2022-01-05 15:39:18 -0500

    ad 5.go TCP port scaner

--- 5.go
+++ 5.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	IP := "scanme.nmap.org"
-	Port := "80"
+	//Port := "80"
 
-	address := IP + ":" + Port
-	connection, err := net.Dial("tcp", address)
-	if err == nil {
-		fmt.Println("[+] Connection established..", connection.RemoteAddr().String())
+	for i := 0; i < 100; i++ {
+		//address := IP + ":" + string(i)
+		address := fmt.Sprintf(IP+":%d", i)
+		connection, err := net.Dial("tcp", address)
+		if err == nil {
+			fmt.Printf("[+] Connection established.. HOST and PORT: %v %v\n", i, connection.RemoteAddr().String())
+		}
+
 	}
 }
