e735888b1302a24e953c36e9ee9baf7a10b1349c
sojoudian <sojodyan@gmail.com>
2022-01-06 23:28:33 -0500

    add 7.go subnet scaner

---
+++ 7.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Ullaakut/nmap"
+)
+
+func main() {
+	targetIP := "10.0.0.0/24"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(targetIP),
+		nmap.WithPorts("80,443,843,8080"),
+		nmap.WithContext(ctx),
+	)
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	results, warning, err := scanner.Run()
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+	if warning != nil {
+		log.Fatal("warning: ", warning)
+	}
+	for _, host := range results.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+		fmt.Printf("IP: %q ", host.Addresses[0])
+		if len(host.Addresses) > 1 {
+			fmt.Printf("MAC %v\n", host.Addresses[1])
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/sojoudian/goSec/v0
+
+go 1.17
+
+require github.com/Ullaakut/nmap v2.0.2+incompatible
+
+require github.com/stretchr/testify v1.7.0 // indirect
