1c2d8ae023afbd7e60b8367e5363bed1662a34fc
sojoudian <sojodyan@gmail.com>
2022-01-09 21:52:22 -0500

    captur all  packets

--- 9.go
+++ 9.go
@@ -10,6 +10,10 @@ import (
 var {
 	iface = "en0"
 	devFound = false
+	snaplen = int32(1600)
+	timeout = pcap.BlockForever
+	filter = "tcp and port 443"
+	promisc = false
 }
 
 func main() {
@@ -23,6 +27,21 @@ func main() {
 		}
 		if !devFound {
 			log.Panicf("Device not found %s", iface)
+		}
+		handle, err := pcap.OpenDev(iface, snaplen, promisc, timeout)
+		if err != nil {
+			log.Panic(err)
+		}
+		// TODO:
+		defer handle.Close()
+		err := handle.SetBPFFilter(filter)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		src := gopacket.NewPacketSource(handle, handle.LinkType())
+		for pkt := range src.Packets {
+			fmt.Println(pkt)
 		}
 	}
 }
