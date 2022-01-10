c2639e7910533cdd4c11b60703d4ee2928894de5
sojoudian <sojodyan@gmail.com>
2022-01-09 22:18:14 -0500

    captur all  packets

--- 9.go
+++ 9.go
@@ -4,43 +4,44 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
-var {
-	iface = "en0"
+var (
+	iface    = "en0"
 	devFound = false
-	snaplen = int32(1600)
-	timeout = pcap.BlockForever
-	filter = "tcp and port 443"
-	promisc = false
-}
+	snaplen  = int32(1600)
+	timeout  = pcap.BlockForever
+	filter   = "tcp and port 443"
+	promisc  = false
+)
 
 func main() {
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
 		log.Panic(err)
 	}
-	for _, device := range devices {
+	for _, dev := range devices {
 		if dev.Name == iface {
 			devFound = true
 		}
 		if !devFound {
 			log.Panicf("Device not found %s", iface)
 		}
-		handle, err := pcap.OpenDev(iface, snaplen, promisc, timeout)
+		handle, err := pcap.OpenLive(iface, snaplen, promisc, timeout)
 		if err != nil {
 			log.Panic(err)
 		}
 		// TODO:
 		defer handle.Close()
-		err := handle.SetBPFFilter(filter)
+		errNew := handle.SetBPFFilter(filter)
 		if err != nil {
-			log.Panic(err)
+			log.Panic(errNew)
 		}
 
 		src := gopacket.NewPacketSource(handle, handle.LinkType())
-		for pkt := range src.Packets {
+		for pkt := range src.Packets() {
 			fmt.Println(pkt)
 		}
 	}
